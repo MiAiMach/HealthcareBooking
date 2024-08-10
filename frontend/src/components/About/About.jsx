9da179a0eb1cc4f82d0c002eca390ad691e2beba
BITHscuits <machm.35489@gmail.com>
2024-08-10 18:10:05 -0500

    Doctors section frontend

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
 import aboutImg from '../../assets/images/about.png';
 import aboutCardImg from '../../assets/images/about-card.png';
+import { Link } from 'react-router-dom';
 
 const About = () => {
   return (
     <section>
       <div className="container">
-        <div className="flex flex-col lg:flex-row lg:items-center gap-[50px] lg:gap-[130px]">
+        <div className="flex justify-between gap-[50px] lg:gap-[130px] xl:gap-0 flex-col lg:flex-row">
           {/* ==========about img ============== */}
-          <div className="relative flex-shrink-0 w-full lg:w-1/2">
-            <img src={aboutImg} alt='' className="w-full h-auto" />
-            <div className="absolute z-20 bottom-4 w-[200px] md:w-[300px] right-[-30%] md:right-[-7%] lg:right-[22%]">
+          <div className="relative w-3/4 lg:w-1/2 xl:w-[770px] z-10 order-2 lg:order-1">
+            <img src={aboutImg} alt='' />
+            <div className="absolute z-20 bottom-4 w-[200px] md:w-[300px] right-[-30%] md:right-[-30%] md:right-[-7%] lg:right-[22%]">
               <img src={aboutCardImg} alt='' />
             </div>
           </div>
           {/* ======= about content ========== */}
-          <div className="flex-1 lg:w-1/2">
-            <h2 className="text-[44px] leading-[54px] font-[700] text-headingColor">
+          <div className="w-full lg:w-1/2 xl:w-[670px] order-1 lg:order-2">
+            <h2 className="heading">
               Proud to be one of the nation's best
             </h2>
-            <p className="text__para">
+            <p className="text__para text-textColor">
               For 30 years in a row, U.S. News & World Report has recognized us as one of the best public hospitals in the Nation and #1 in Texas. Lorem ipsum dolor sit amet consectetur, adipiscing elit. Quas, nemo?
             </p>
+            <p className="text__para text-textColor mt-[30px]">Our best is something we strive for each day, caring for our patients- not looking back at what we accomplished but towards what we can do tomorrow. Provind the best. Lorem ipsum dolor sit amet, cosecutetur adipisicing elit. Aliquid, modi?</p>
+            <Link to='/'>
+              <button className="btn">Learn More</button>
+            </Link>
           </div>
         </div>
       </div>
